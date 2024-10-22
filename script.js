3bee7aae665654b86f924a8165f850d176c44539
RobLins12 <robson.2021104910@unicap.br>
2024-10-22 10:30:34 -0300

    adicionando uma splash screen

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        window.location.href = "lib/view/index.html"
+    }, 3000); // Adjust the timeout to match the animation duration
+});
